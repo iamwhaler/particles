85918a1dc679b2fda79a2763f29612bdd0aa56df
Alex Gapak <iamgapak@gmail.com>
2018-09-09 22:17:36 +0300

    Add Neutronator

--- src/game/fluctuators.js
+++ src/game/fluctuators.js
@@ -64,11 +64,98 @@ const assemble_helper = (state, name) => {
 
 export const fluctuators = {
     modules: {
+
+        polarizer: {
+            name: 'Polarizer',
+            text: 'Attracts Electrons and Protons from space',
+            cost: (state) => {
+                return {
+                    'field.protons': Math.floor(Math.pow(1.5, state.polarizer) * 100)}
+            },
+
+            locked: (state) => false,
+
+            temperature_effect: (state) => {
+                return Math.floor(Math.pow(1.3, state.polarizer - 1) * 10);
+            },
+
+            toggle: (state) => toggle_helper(state, 'polarizer'),
+
+            onClick: (state) => {
+                state.polarizer++;
+                return state;
+            },
+            onTick: (state) => {
+                const name = 'polarizer';
+                if (state.toggle[name] === false) return state;
+                let electrons_count = Math.round(_.random(state.polarizer/3, state.polarizer));
+                let protons_count = Math.round(_.random(state.polarizer/4, state.polarizer));
+
+                if (state.space.electrons > electrons_count && state.space.protons > protons_count
+                    && state.toggle[name] && state.polarizer >= 1
+                    && state.space.electrons > state.pump
+                    && state.space.protons > state.pump) {
+                    if (state.field_capacity < weight({electrons: electrons_count, protons: protons_count}) + weight(state.field)) state.toggle[name] = false;
+                    state.field.electrons += electrons_count;
+                    state.space.electrons -= electrons_count;
+                    state.field.protons += protons_count;
+                    state.space.protons -= protons_count;
+                }
+                else {
+                    state.toggle[name] = false;
+                }
+
+                return state;
+            }
+        },
+
+        protonator: {
+            name: 'Protonator',
+            text: 'Converts Neutron and Neutrino into Electron and Proton',
+            cost: (state) => {
+                return {'field.electrons': Math.floor(Math.pow(1.5, state.protonator) * 250)}
+            },
+
+            locked: (state) => false,
+
+            temperature_effect: (state) => {
+                return Math.floor(Math.pow(1.3, state.protonator - 1) * 10);
+            },
+
+            toggle: (state) => toggle_helper(state, 'protonator'),
+            onClick: (state) => {
+                state.protonator++;
+                return state;
+            },
+            onTick: (state) => {
+                const name = 'protonator';
+                if (state.toggle[name] === false) return state;
+                let electrons_count = state.protonator *2;
+                let protons_count = state.protonator *2;
+                if (state.field.neutrino > electrons_count && state.field.neutrons > protons_count
+                    && state.protonator >= 1
+                    && state.space.electrons>state.protonator
+                    && state.space.photons>state.protonator) {
+
+                    if (state.field_capacity < weight({electrons: electrons_count, protons: protons_count}) + weight(state.field)) state.toggle[name] = false;
+                    state.field.electrons += electrons_count;
+                    state.field.protons += protons_count;
+                    state.field.neutrino -= electrons_count;
+                    state.field.neutrons -= protons_count;
+                }
+                else {
+                    state.toggle[name] = false;
+                }
+
+                return state;
+            }
+        },
+
         panel: {
             name: 'Panel',
             text: 'Attracts Photons and Neutrino from space',
             cost: (state) => {
-                return{ 'field.neutrino': Math.floor(Math.pow(1.1, state.panel -1) *4)}
+                return{ 'field.photons': Math.floor(Math.pow(1.5, state.panel) * 100)}
             },
             locked: (state) => false,
 
@@ -102,11 +189,52 @@ export const fluctuators = {
             }
         },
 
+        neutronator: {
+            name: 'Neutronator',
+            text: 'Converts Electron and Proton into Neutron and Neutrino',
+            cost: (state) => {
+                return {'field.neutrino': Math.floor(Math.pow(1.5, state.protonator) * 250)}
+            },
+
+            locked: (state) => false,
+
+            temperature_effect: (state) => {
+                return Math.floor(Math.pow(1.3, state.protonator - 1) * 10);
+            },
+            toggle: (state) => toggle_helper(state, 'neutronator'),
+            onClick: (state) => {
+                state.neutronator++;
+                return state;
+            },
+            onTick: (state) => {
+                const name = 'neutronator';
+                if (state.toggle[name] === false) return state;
+                let electrons_count = state.protonator *2;
+                let protons_count = state.protonator *2;
+                if (state.field.neutrino > electrons_count && state.field.neutrons > protons_count
+                    && state.protonator >= 1
+                    && state.space.electrons > state.protonator
+                    && state.space.photons > state.protonator) {
+
+                    if (state.field_capacity < weight({electrons: electrons_count, protons: protons_count}) + weight(state.field)) state.toggle[name] = false;
+                    state.field.electrons -= electrons_count;
+                    state.field.protons -= protons_count;
+                    state.field.neutrino += electrons_count;
+                    state.field.neutrons += protons_count;
+                }
+                else {
+                    state.toggle[name] = false;
+                }
+
+                return state;
+            }
+        },
+
         pump: {
             name: 'Matter Pump',
             text: 'Sucks Neutrons and atoms from space',
             cost: (state) => {
-                return{ 'field.neutrons': Math.floor(Math.pow(1.1, state.pump -1) *4)}
+                return{ 'field.neutrons': Math.floor(Math.pow(1.5, state.pump) * 100)}
             },
             locked: (state) => false,
 
@@ -152,108 +280,13 @@ export const fluctuators = {
                 return state;
             }
         },
-
-        polarizer: {
-            name: 'Polarizer',
-            text: 'Attracts Electrons and Protons from space',
-            cost: (state) => {
-                return {
-                    'field.protons': Math.floor(Math.pow(1.5, state.polarizer - 1) * 2)}
-            },
-
-            locked: (state) => false,
-
-            temperature_effect: (state) => {
-                return Math.floor(Math.pow(1.3, state.polarizer - 1) * 10);
-            },
-
-            toggle: (state) => toggle_helper(state, 'polarizer'),
-
-            onClick: (state) => {
-                state.polarizer++;
-                return state;
-            },
-            onTick: (state) => {
-                const name = 'polarizer';
-                if (state.toggle[name] === false) return state;
-                let electrons_count = Math.round(_.random(state.polarizer/3, state.polarizer));
-                let protons_count = Math.round(_.random(state.polarizer/4, state.polarizer));
-
-                if (state.space.electrons > electrons_count && state.space.protons > protons_count
-                    && state.toggle.polarizer && state.polarizer >= 1
-                    && state.space.electrons > state.pump
-                    && state.space.protons > state.pump) {
-                    if (state.field_capacity < weight({electrons: electrons_count, protons: protons_count}) + weight(state.field)) state.toggle[name] = false;
-                    state.field.electrons += electrons_count;
-                    state.space.electrons -= electrons_count;
-                    state.field.protons += protons_count;
-                    state.space.protons -= protons_count;
-                }
-                else {
-                    state.toggle[name] = false;
-                }
-
-                return state;
-            }
-        },
-
-
-        protonator: {
-            name: 'Protonator',
-            text: 'Converts Neutron and Neutrino into Electron and Proton',
-            cost: (state) => {
-                return {'field.photons': Math.floor(Math.pow(1.2, state.protonator - 1) * 4)}
-            },
-
-            locked: (state) => false,
-
-            temperature_effect: (state) => {
-                return Math.floor(Math.pow(1.3, state.protonator - 1) * 10);
-            },
-
-            toggle: (state) => {
-                (state.toggle.protonator)
-                    ? state.toggle.protonator=false
-                    : state.toggle.protonator=true;
-
-                return state;
-            },
-            onClick: (state) => {
-                state.protonator++;
-                return state;
-            },
-            onTick: (state) => {
-                const name = 'protonator';
-                if (state.toggle[name] === false) return state;
-                let electrons_count = state.protonator *2;
-                let protons_count = state.protonator *2;
-                if (state.field.neutrino > electrons_count && state.field.neutrons > protons_count
-                    && state.toggle.protonator && state.protonator >= 1
-                    && state.space.electrons>state.protonator
-                    && state.space.photons>state.protonator) {
-
-                    if (state.field_capacity < weight({electrons: electrons_count, protons: protons_count}) + weight(state.field)) state.toggle[name] = false;
-                    state.field.electrons += electrons_count;
-                    state.field.protons += protons_count;
-                    state.field.neutrino -= electrons_count;
-                    state.field.neutrons -= protons_count;
-                }
-                else {
-                    state.toggle[name] = false;
-                }
-
-                return state;
-            }
-        },
-
-
     },
 
     assemblers: {
         hydrogen: {
             name: 'Hydrogen Assembler',
             text: 'Synths Hydrogen consuming elementary particles',
-            cost: (state) => {return {'storage.hydrogen': Math.floor(Math.pow(1.7, state.assemblers.hydrogen - 1) * 20)};},
+            cost: (state) => {return {'storage.hydrogen': Math.floor(Math.pow(1.5, state.assemblers.hydrogen) * 1000)};},
             locked: (state) => false,
             toggle: (state) => toggle_helper(state, 'hydrogen'),
             onClick: (state) => buy_assembler_helper(state, 'hydrogen'),
@@ -263,7 +296,7 @@ export const fluctuators = {
         helium: {
             name: 'Helium Assembler',
             text: 'Synths Hydrogen consuming elementary particles',
-            cost: (state) => {return {'storage.helium': Math.floor(Math.pow(1.7, state.assemblers.helium - 1) * 20)};},
+            cost: (state) => {return {'storage.helium': Math.floor(Math.pow(1.6, state.assemblers.helium) * 1000)};},
             locked: (state) => false,
             toggle: (state) => toggle_helper(state, 'helium'),
             onClick: (state) => buy_assembler_helper(state, 'helium'),
@@ -273,7 +306,7 @@ export const fluctuators = {
         carbon: {
             name: 'Carbon Assembler',
             text: 'Synths Hydrogen consuming elementary particles',
-            cost: (state) => {return {'storage.carbon': Math.floor(Math.pow(1.7, state.assemblers.carbon - 1) * 20)};},
+            cost: (state) => {return {'storage.carbon': Math.floor(Math.pow(1.7, state.assemblers.carbon) * 1000)};},
             locked: (state) => false,
             toggle: (state) => toggle_helper(state, 'carbon'),
             onClick: (state) => buy_assembler_helper(state, 'carbon'),
@@ -283,7 +316,7 @@ export const fluctuators = {
         oxygen: {
             name: 'Oxygen Assembler',
             text: 'Synths Hydrogen consuming elementary particles',
-            cost: (state) => {return {'storage.oxygen': Math.floor(Math.pow(1.7, state.assemblers.oxygen - 1) * 20)};},
+            cost: (state) => {return {'storage.oxygen': Math.floor(Math.pow(1.8, state.assemblers.oxygen) * 1000)};},
             locked: (state) => false,
             toggle: (state) => toggle_helper(state, 'oxygen'),
             onClick: (state) => buy_assembler_helper(state, 'oxygen'),
@@ -293,7 +326,7 @@ export const fluctuators = {
         nitrogen: {
             name: 'Nitrogen Assembler',
             text: 'Synths Hydrogen consuming elementary particles',
-            cost: (state) => {return {'storage.nitrogen': Math.floor(Math.pow(1.7, state.assemblers.nitrogen - 1) * 20)};},
+            cost: (state) => {return {'storage.nitrogen': Math.floor(Math.pow(1.9, state.assemblers.nitrogen) * 1000)};},
             locked: (state) => false,
             toggle: (state) => toggle_helper(state, 'nitrogen'),
             onClick: (state) => buy_assembler_helper(state, 'nitrogen'),
@@ -303,7 +336,7 @@ export const fluctuators = {
         aluminium: {
             name: 'Aluminium Assembler',
             text: 'Synths Aluminium consuming elementary particles',
-            cost: (state) => {return {'storage.aluminium': Math.floor(Math.pow(1.7, state.assemblers.aluminium - 1) * 20)};},
+            cost: (state) => {return {'storage.aluminium': Math.floor(Math.pow(2, state.assemblers.aluminium) * 1000)};},
             locked: (state) => false,
             toggle: (state) => toggle_helper(state, 'aluminium'),
             onClick: (state) => buy_assembler_helper(state, 'aluminium'),
@@ -313,7 +346,7 @@ export const fluctuators = {
         silicon: {
             name: 'Silicon Assembler',
             text: 'Synths Hydrogen consuming elementary particles',
-            cost: (state) => {return {'storage.silicon': Math.floor(Math.pow(1.7, state.assemblers.silicon - 1) * 20)};},
+            cost: (state) => {return {'storage.silicon': Math.floor(Math.pow(2.1, state.assemblers.silicon) * 1000)};},
             locked: (state) => false,
             toggle: (state) => toggle_helper(state, 'silicon'),
             onClick: (state) => buy_assembler_helper(state, 'silicon'),
@@ -323,126 +356,12 @@ export const fluctuators = {
         ferrum: {
             name: 'Ferrum Assembler',
             text: 'Synths Hydrogen consuming elementary particles',
-            cost: (state) => {return {'storage.ferrum': Math.floor(Math.pow(1.7, state.assemblers.ferrum - 1) * 20)};},
+            cost: (state) => {return {'storage.ferrum': Math.floor(Math.pow(2.2, state.assemblers.ferrum) * 1000)};},
             locked: (state) => false,
             toggle: (state) => toggle_helper(state, 'ferrum'),
             onClick: (state) => buy_assembler_helper(state, 'ferrum'),
             onTick: (state) => assemble_helper(state, 'ferrum')
         },
-
-
-        /*
-        old_hydrogen_assembler: {
-            name: 'H2 Assembler',
-            text: 'Synths Hydrogen consuming elementary particles',
-            cost: (state) => {
-                return {
-                    'storage.hydrogen': Math.floor(Math.pow(1.7, state.hydrogen - 1) * 20),
-                };
-            },
-            locked: (state) => false,
-            toggle: (state) => {
-                (state.toggle.hydrogen_assembler)
-                    ? state.toggle.hydrogen_assembler=false
-                    : state.toggle.hydrogen_assembler=true;
-                return state;
-            },
-            onClick: (state) => {
-                state.hydrogen_assembler++;
-                return state;
-            },
-            onTick: (state) => {
-                let count = state.hydrogen * 2;
-                if (state.field.photons > count && state.field.electrons > count
-                    && state.field.neutrons > count && state.field.protons > count && state.toggle.hydrogen_assembler
-                    && state.hydrogen >= 1) {
-                    state.storage.hydrogen += count;
-                    state.field.protons -= count;
-                    state.field.neutrons -= count;
-                    state.field.electrons -= count;
-                    state.field.photons -= count;
-
-                }
-                return state;
-
-            }
-        },
-
-        old_helium_assembler: {
-            name: 'Helium Assembler',
-            text: 'Synths Helium from existing particles',
-            cost: (state) => {
-                return {
-                    "storage.helium": Math.floor(Math.pow(1.7, state.helium - 1) * 20),
-                };
-            },
-            locked: (state) => false,
-            toggle: (state) => {
-                (state.toggle.helium_assembler)
-                    ? state.toggle.helium_assembler=false
-                    : state.toggle.helium_assembler=true;
-                return state;
-            },
-            onClick: (state) => {
-                state.helium_assembler++;
-                return state;
-            },
-            onTick: (state) => {
-                let count = (state.helium * 2);
-                if (state.field.protons > count*2 && state.field.photons > count*2
-                    && state.field.electrons > count*2
-                    && state.field.neutrons > count*2
-                    && state.toggle.helium && state.helium >= 1
-                    && state.tick % Math.floor(info.helium.mass) === 0) {
-                    state.storage.helium += count;
-                    state.field.protons -= count*2;
-                    state.field.neutrons -= count*2;
-                    state.field.electrons -= count*2;
-                    state.field.photons -= count*2;
-
-                }
-                return state;
-
-            }
-        },
-
-        old_carbon_assembler: {
-            name: 'Carbon Assembler',
-            text: 'Synths Carbon from existing particles',
-            cost: (state) => {
-                return {
-                    "storage.carbon": Math.floor(Math.pow(1.7, state.carbon - 1) * 20),
-                };
-            },
-            locked: (state) => false,
-            toggle: (state) => {
-                (state.toggle.carbon_assembler)
-                    ? state.toggle.carbon_assembler=false
-                    : state.toggle.carbon_assembler=true;
-                return state;
-            },
-            onClick: (state) => {
-                state.carbon_assembler++;
-                return state;
-            },
-            onTick: (state) => {
-                let count = (state.carbon * 2);
-                if (state.field.protons > count*6 && state.field.photons > count*6
-                    && state.field.electrons > count*6
-                    && state.field.neutrons > count*6
-                    && state.toggle.carbon && state.carbon >= 1
-                    && state.tick % Math.floor(info.carbon.mass) === 0) {
-                    state.storage.carbon += count;
-                    state.field.protons -= count*6;
-                    state.field.neutrons -= count*6;
-                    state.field.electrons -= count*6;
-                    state.field.photons -= count*6;
-                }
-                return state;
-
-            }
-        },
-        */
 
     }
 };
